86137cdeeb8591af1fdad7a882a347725f0b4972
sabari pv <sabariprakashpv@gmail.com>
2026-04-28 14:56:54 +0530

    added requirements.txt correctly

--- food_backend/settings.py
+++ food_backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-392sfoh_!)7tvtft&_r*xmkd0$(7qof!nxo%5%y#^sdc6ic2++'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 CORS_ALLOW_ALL_ORIGINS = True
 
